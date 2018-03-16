67fd1165c41a80e37f88d30a73a37ad131d6cc0b
matheus-lima92 <lima.eca@gmail.com>
2018-03-16 18:25:03 -0300

    css changes, and clear button

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -19,6 +19,7 @@ export default class Todo extends Component{
 		this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
 		this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
 		this.handleSearch = this.handleSearch.bind(this)
+		this.handleClear = this.handleClear.bind(this)
 		this.refresh()
 
 	}
@@ -54,6 +55,10 @@ export default class Todo extends Component{
 		axios.put(`${API_URL}/${todo._id}`, { ...todo, done: false }).then(resp => this.refresh(this.state.description))
 	}
 
+	handleClear(){
+		this.refresh()
+	}
+
 	render(){
 		return (
 			<div>
@@ -63,6 +68,7 @@ export default class Todo extends Component{
 					handleChange={this.handleChange} 
 					handleAdd={this.handleAdd}
 					handleSearch={this.handleSearch}
+					handleClear={this.handleClear}
 				/>
 				<TodoList 
 					list={this.state.list} 
